c3348a0404ea6340dffc62d53e581b5d1a64b6e4
tebmik <lloyd.s.tom@gmail.com>
2020-05-30 10:42:11 +0100

    Navbar component added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Navbar from "./navbar/Navbar";
+
 
 const Main = () => {
     return (
-        <div>Main</div>
+        <div>
+            <Navbar />
+        </div>
     );
 };
 

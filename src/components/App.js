8d8f360b95f90a13fcd3db49cf52423deb0bc1e9
tebmik <lloyd.s.tom@gmail.com>
2020-05-30 10:39:11 +0100

    Main.js component add and imported to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Main from "./Main";
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Main />
+        </div>
     )
 };
 

5f80383bee3692fa1267c2d4b29895128c7e2519
tebmik <lloyd.s.tom@gmail.com>
2020-05-30 11:22:02 +0100

    ThemeProvider import from styled-components dependancy, this will interact with entire application

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./utils/theme";
+import GlobalStyles from "./utils/global";
+
+
+
 
 ReactDOM.render(
-    <App />,
+    <ThemeProvider theme={theme}>
+        <>
+            <App />
+            <GlobalStyles />
+        </>
+    </ThemeProvider>,
     document.querySelector("#root")
 );

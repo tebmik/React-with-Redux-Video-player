2a02f446833148b2dc4f418096bc0d64c0ad663b
tebmik <lloyd.s.tom@gmail.com>
2020-05-31 15:08:09 +0100

    http request made through fetchMedia action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import { FETCH_MEDIA } from "./types"
+import { KEY } from "../config/config";
 
 export const fetchMedia = () => async dispatch => {
-    const response = await youtubeApi.get("/search")
+    const response = await youtubeApi.get("/search", {
+        params: {
+            part: "snippet",
+            maxResult: 5,
+            key: KEY
+        }
+    })
     dispatch({ type: FETCH_MEDIA, payload: response });
 };

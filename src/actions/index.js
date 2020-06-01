f259a3ac0b5123e3b679b84c84a06b5896facedd
tebmik <lloyd.s.tom@gmail.com>
2020-06-01 13:41:19 +0100

    try/catch statement included with response.data added to payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,16 @@ import { KEY } from "../config/config";
 import youtubeApi from "../api/youtubeApi";
 
 export const fetchMedia = () => async dispatch => {
-    const response = await youtubeApi.get("/search", {
-        params: {
-            part: "snippet",
-            maxResult: 1,
-            key: KEY
-        }
-    })
-    dispatch({ type: FETCH_MEDIA, payload: response });
+    try {
+        const response = await youtubeApi.get("/search", {
+            params: {
+                part: "snippet",
+                maxResult: 1,
+                key: KEY
+            }
+        });
+        dispatch({ type: FETCH_MEDIA, payload: response.data });
+    } catch(err) {
+        console.log("something went wrong")
+    }
 };
